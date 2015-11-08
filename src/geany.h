07af4214cac02cb5ab99cdd5b6e084a8b4ba4c4c
PaulSJ <PaulSJ@users.noreply.github.com>
2015-11-08 03:55:06 +0000

    Version 2.0.15c changes
    Version 2.0.15c changes

--- src/geany.h
+++ src/geany.h
@@ -55,8 +55,8 @@ G_BEGIN_DECLS
 
 // check these three for new versions
 //#define AGK_FREE_VERSION
-#define AGK_VERSION_TEXT "2.0.15"
-#define AGK_VERSION_INT 10
+#define AGK_VERSION_TEXT "2.0.15c"
+#define AGK_VERSION_INT 12
 
 
 #ifdef AGK_FREE_VERSION
