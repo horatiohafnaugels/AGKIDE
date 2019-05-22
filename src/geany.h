e9fd21e3aae1f279ae9f7923d3b769452432be94
PaulSJ <paul@thegamecreators.com>
2019-05-22 14:09:54 +0100

    Version 2019.05.22 Changes
    Removed generated files

--- src/geany.h
+++ src/geany.h
@@ -56,8 +56,8 @@ G_BEGIN_DECLS
 // check these three for new versions
 //#define AGK_FREE_VERSION
 //#define AGK_WEEKEND_VERSION
-#define AGK_VERSION_TEXT "2019.02.18"
-#define AGK_VERSION_INT 46
+#define AGK_VERSION_TEXT "2019.05.22"
+#define AGK_VERSION_INT 47
 
 
 #ifdef AGK_FREE_VERSION
