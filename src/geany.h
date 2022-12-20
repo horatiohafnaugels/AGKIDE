daacfb3d5ffc0ec6801213198bbb17c426096d91
PaulSJ <PaulSJ@users.noreply.github.com>
2022-12-20 01:10:28 +0000

    Version 2022.12.13 Changes

--- src/geany.h
+++ src/geany.h
@@ -56,8 +56,8 @@ G_BEGIN_DECLS
 // check these three for new versions
 //#define AGK_FREE_VERSION
 //#define AGK_WEEKEND_VERSION
-#define AGK_VERSION_TEXT "2022.09.28"
-#define AGK_VERSION_INT 61
+#define AGK_VERSION_TEXT "2022.12.13"
+#define AGK_VERSION_INT 62
 
 
 #ifdef AGK_FREE_VERSION
